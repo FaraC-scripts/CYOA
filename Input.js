56acf4f5843f56d32cdeda7fab6e0001c9a626fd
FaraC <pharmergency@gmail.com>
2025-12-23 14:37:12 -0800

    Create Input.js

---
+++ Input.js
@@ -0,0 +1,36 @@
+const modifier = (text) => {
+  const regex = /\n? ?(?:> You |> You say "|)\/(\w+?)( [\w ]+)?[".]?\n?$/i
+  const commandMatcher = text.match(regex)
+  state.cyoaLine = "\n> Select the next story event by inputting \"/a\", \"/b\", \"/c\", or \"/d\""
+  let command = null;
+  let args = null;
+  let newText = text;
+  let stop = false;
+  const modeMax = 2
+
+  if (commandMatcher) {
+    command = commandMatcher[1].toLowerCase()
+    args = commandMatcher[2] ? commandMatcher[2].toLowerCase().trim().split(' ') : []
+  }
+  if(command === "cyoa"){
+    state.cyoa = true;
+    newText = state.cyoaLine
+  }
+  if(command === "a" && state.latestCyoa){
+    newText = "\n\n["+findLastLineStartingWith(state.latestCyoa, "A.").substring(3)+"]\n"
+    state.cyoa = false;
+  }
+  if(command === "b" && state.latestCyoa){
+    newText = "\n\n["+findLastLineStartingWith(state.latestCyoa, "B.").substring(3)+"]\n"
+    state.cyoa = false;
+  }
+  if(command === "c" && state.latestCyoa){
+    newText = "\n\n["+findLastLineStartingWith(state.latestCyoa, "C.").substring(3)+"]\n"
+    state.cyoa = false;
+  }
+  if(command === "d" && state.latestCyoa){
+    newText = "\n\n["+findLastLineStartingWith(state.latestCyoa, "D.").substring(3)+"]\n"
+    state.cyoa = false;
+  }
+  return { text: newText, stop }
+}
