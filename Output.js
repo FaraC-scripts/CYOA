b70c6e36cf57a31ff5c99b0aa70e42f152b0d781
FaraC <pharmergency@gmail.com>
2025-12-23 14:48:11 -0800

    Create Output.js

---
+++ Output.js
@@ -0,0 +1,18 @@
+const modifier = (text) => {
+    if(state.cyoa){
+        if (newText.startsWith('\n')){
+            newText = newText.substring(1)
+        }
+        if (newText.endsWith('\n')){
+            newText = newText.slice(0, -1)
+        }
+        newText = newText.trim()
+        state.latestCyoa = newText
+        newText = addSymbolToString(newText, "• ")
+    }
+
+    return { text : newText }
+}
+
+// Don't modify this part
+modifier(text)
