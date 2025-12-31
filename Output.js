475a338eb3d3d8bb0349a53097e4a106ededb7ca
FaraC <pharmergency@gmail.com>
2025-12-31 07:59:04 -0800

    code cleanup

--- Output.js
+++ Output.js
@@ -1,19 +1,6 @@
 const modifier = (text) => {
-    let newText = text
-    if(state.cyoa){
-        if (newText.startsWith('\n')){
-            newText = newText.substring(1)
-        }
-        if (newText.endsWith('\n')){
-            newText = newText.slice(0, -1)
-        }
-        newText = newText.trim()
-        state.latestCyoa = newText
-        newText = addSymbolToString(newText, "• ")
-    }
-
-    return { text : newText }
+  text = handleCyoaOutput(text)
+  return { text }
 }
 
-// Don't modify this part
 modifier(text)
